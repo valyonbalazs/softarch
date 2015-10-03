001d21ca506499a412596851afe578f43b30dc15
Valyon Balázs <valyon.balazs@gmail.com>
2015-10-03 17:03:43 +0200

    working: getting file list and metadata from google drive

--- test_code/js/request.js
+++ test_code/js/request.js
@@ -1,45 +1,58 @@
+var CLIENT_ID = '268864776090-ltnggc4pcsd411f9ngbi3008ogkdbb34.apps.googleusercontent.com';
+var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 
+function checkAuth() {
+  gapi.auth.authorize(
+    {
+      'client_id': CLIENT_ID,
+      'scope': SCOPES.join(' '),
+      'immediate': true
+    }, handleAuthResult);
+}
 
-
-function getDriveFileList () {
-    function retrieveAllFiles(callback) {
-    var retrievePageOfFiles = function(request, result) {
-      request.execute(function(resp) {
-        result = result.concat(resp.items);
-        console.log(result);
-        var nextPageToken = resp.nextPageToken;
-        if (nextPageToken) {
-          request = gapi.client.drive.files.list({
-            'pageToken': nextPageToken
-          });
-          retrievePageOfFiles(request, result);
-        } else {
-          callback(result);
-        }
-      });
-    }
-    var initialRequest = gapi.client.drive.files.list();
-    retrievePageOfFiles(initialRequest, []);
+function handleAuthResult(authResult) {
+  var authorizeDiv = document.getElementById('authorize-div');
+  if (authResult && !authResult.error) {
+    authorizeDiv.style.display = 'none';
+    loadDriveApi();
+  } else {
+    authorizeDiv.style.display = 'inline';
   }
 }
 
-function retrieveAllFiles (callback) {
+function handleAuthClick(event) {
+  gapi.auth.authorize(
+    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+    handleAuthResult);
+  return false;
+}
 
-  var retrievePageOfFiles = function (request, result) {
+function loadDriveApi() {
+  gapi.client.load('drive', 'v2', listFiles);
+}
 
-    request.execute( function (resp) {
-      result = result.concat(resp.items);
-      var nextPageToken = resp.nextPageToken;
-      if (nextPageToken) {
-        request = gapi.client.drive.files.list({
-          'pageToken': nextPageToken
-        });
-        retrievePageOfFiles(request, result);
+function listFiles() {
+  var request = gapi.client.drive.files.list({
+      'maxResults': 100
+    });
+
+    request.execute(function(resp) {
+      appendPre('Last 100 files from your google drive:');
+      var files = resp.items;
+      if (files && files.length > 0) {
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          console.log(file);
+          appendPre('NAME: ' + file.title + '  CREATED ON: ' + file.createdDate + ' BY ' + file.lastModifyingUserName);
+        }
       } else {
-        callback(result);
+        appendPre('No files found.');
       }
     });
-  }
-  var initialRequest = gapi.client.drive.files.list();
-  retrievePageOfFiles(initialRequest, []);
+}
+
+function appendPre(message) {
+  var pre = document.getElementById('output');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
 }

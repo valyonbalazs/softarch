90e7c0edf4abbc59c5a24d5ca2fb3d1bcb045190
Valyon Balázs <valyon.balazs@gmail.com>
2015-10-11 18:06:09 +0200

    risk modal window gets data, tasks status dropdown list

--- test_code/js/angularData.js
+++ test_code/js/angularData.js
@@ -18,7 +18,23 @@ projectApp.factory('taskDataSharerService', function ($rootScope) {
   return service;
 });
 
-projectApp.controller('ProjectController', ['$scope', 'taskDataSharerService', function ($scope, taskDataSharerService) {
+var modalRiskData = {
+  riskName: ''
+};
+projectApp.factory('riskDataSharerService', function ($rootScope) {
+  var service = {};
+  service.risk = modalRiskData;
+  service.sendData = function(data) {
+    this.data = data;
+    $rootScope.$broadcast('riskDataSharerService');
+  };
+  service.getData = function() {
+    return this.data;
+  };
+  return service;
+});
+
+projectApp.controller('ProjectController', ['$scope', function ($scope, taskDataSharerService) {
     $scope.project = [
         {
           projectId: 1,
@@ -30,17 +46,7 @@ projectApp.controller('ProjectController', ['$scope', 'taskDataSharerService', f
     ];
   }
 ]);
-/*controllers.ProjectController = function ($scope) {
-  $scope.project = [
-      {
-        projectId: 1,
-        projectName: 'Test project',
-        projectLeader: 'Bruce Wayne',
-        projectWasCreated: '2015. October 05.',
-        projectWasLastSaved: '2015. October 10.'
-      }
-  ];
-};*/
+
 projectApp.controller('TaskController', ['$scope', 'taskDataSharerService', function ($scope, taskDataSharerService) {
     $scope.init = function () {
       $scope.tasks = [
@@ -88,53 +94,7 @@ projectApp.controller('TaskController', ['$scope', 'taskDataSharerService', func
     };
   }
 ]);
-/*controllers.TaskController = function ($scope) {
 
-  $scope.init = function () {
-    $scope.tasks = [
-      {
-        taskId: 1,
-        taskName: 'First task',
-        taskStatus: 'In progress',
-        taskStart: '2015. October 03.',
-        taskEnd: '2015. October 12',
-        taskDependencies: 1,
-        taskResponsibles: 'Alfred Pennyworth',
-        taskComments: 'comment1'
-      },
-      {
-        taskId: 2,
-        taskName: 'Second task',
-        taskStatus: 'In progress',
-        taskStart: '2015. October 05.',
-        taskEnd: '2015. October 17',
-        taskDependencies: 1,
-        taskResponsibles: 'Clark Kent',
-        taskComments: 'comment2'
-      },
-      {
-        taskId: 3,
-        taskName: 'Third task',
-        taskStatus: 'Not started',
-        taskStart: '2015. October 22.',
-        taskEnd: '2015. November 07.',
-        taskDependencies: '1, 2',
-        taskResponsibles: 'Barry Allen',
-        taskComments: 'comment3'
-      }
-    ];
-  };
-
-  $scope.modalTaskName = 'empty';
-
-  $scope.setTaskNameForModal = function (id) {
-    console.log(id);
-    var task = $scope.tasks[id-1];
-    $scope.modalTaskName = task.taskName;
-    console.log($scope.modalTaskName);
-    taskDataSharerService.sendData($scope.modalTaskName);
-  };
-};*/
 projectApp.controller('ModalTaskController', ['$scope', 'taskDataSharerService', function ($scope, taskDataSharerService) {
     $scope.taskName = '';
     $scope.$on('taskDataSharerService', function () {
@@ -144,16 +104,17 @@ projectApp.controller('ModalTaskController', ['$scope', 'taskDataSharerService',
     });
   }
 ]);
-/*controllers.ModalTaskController = function ($scope) {
 
-  $scope.taskName = '';
-  $scope.$on('taskDataSharerService', function () {
-    var text = taskDataSharerService.getData();
-    $scope.taskName = text;
-  });
-};*/
+projectApp.controller('ModalRiskController', ['$scope', 'riskDataSharerService', function ($scope, riskDataSharerService) {
+    $scope.riskName = '';
+    $scope.$on('riskDataSharerService', function () {
+      var riskData = riskDataSharerService.getData();
+      $scope.riskName = riskData.riskName;
+    });
+  }
+]);
 
-projectApp.controller('RiskController', ['$scope', 'taskDataSharerService', function ($scope, taskDataSharerService) {
+projectApp.controller('RiskController', ['$scope', 'riskDataSharerService', function ($scope, riskDataSharerService) {
     $scope.risks = [
       {
         riskId: 1,
@@ -166,21 +127,14 @@ projectApp.controller('RiskController', ['$scope', 'taskDataSharerService', func
         riskImportance: 'High risk'
       }
     ];
+
+    $scope.setRiskNameForModal = function (id) {
+      console.log(id);
+      var risk = $scope.risks[id-1];
+      var riskPassableData = {
+        riskName: risk.riskName
+      };
+      riskDataSharerService.sendData(riskPassableData);
+    };
   }
 ]);
-/*controllers.RiskController = function ($scope) {
-  $scope.risks = [
-    {
-      riskId: 1,
-      riskName: 'Name and description of the first risk',
-      riskImportance: 'Low risk'
-    },
-    {
-      riskId: 2,
-      riskName: 'Name and description of the second risk',
-      riskImportance: 'High risk'
-    }
-  ];
-};*/
-
-//projectApp.controller(controllers);
---
+++ test_code/js/view-modification.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".dropdown-menu li a").click(function(){
+    var btn = $(this).parent().parent().prev();
+    console.log(btn[0]);
+    var idOfBtn = $(btn).attr('id');
+    console.log(idOfBtn);
+    $('#'+ idOfBtn).html($(this).text()+' <span class="caret"></span>');
+  });
+});

1ae663fc7cf6045acf6e42371dca9b4984980739
Valyon Balázs <valyon.balazs@gmail.com>
2015-11-10 16:14:56 +0100

    third chart with added work hours and work days

--- angular_grant_src/scripts.js
+++ angular_grant_src/scripts.js
@@ -283,6 +283,8 @@ angular.module('angularGanttDemoApp')
 
           var biggestTo = undefined;
           var tasksDuration = [];
+          var allTaskWorkHours = 0.0;
+          var allTaskWorkDays = 0;
           for(var key in $scope.data) {
             if($scope.data[key].hasOwnProperty('children')) {
 
@@ -296,6 +298,8 @@ angular.module('angularGanttDemoApp')
                 var s = Math.floor(d.asHours());
                 var dayFromS = s/24;
                 var dayFromSPrecised = dayFromS.toPrecision(4);
+                s = dayFromSPrecised * 8;
+                allTaskWorkHours += s;
 
                 var taskTime = {
                   name: $scope.data[key].tasks[t].name,
@@ -331,10 +335,11 @@ angular.module('angularGanttDemoApp')
           		"gridPosition": "start",
           		"position": "left"
           	},
+            "colors": ['#B0DE09'],
           	"trendLines": [],
           	"graphs": [
           		{
-          			"balloonText": "Duration is [[value]] hours which is [[description]] days",
+          			"balloonText": "Duration is [[value]] WORK hours which is [[description]] days",
           			"fillAlphas": 0.8,
           			"id": "AmGraph-1",
           			"lineAlpha": 0.2,
@@ -369,12 +374,63 @@ angular.module('angularGanttDemoApp')
           "titleField": "name",
           "outlineAlpha": 0.4,
           "depth3D": 15,
-          "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+          "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> days ([[percents]]%)</span>",
           "angle": 30,
           "export": {
             "enabled": true
           }
         } );
+
+        allTaskWorkDays = allTaskWorkHours/8;
+        allTaskWorkDays = allTaskWorkDays.toPrecision(3);
+        var allTaskWorkHoursObject =
+        [
+          {
+            hours: allTaskWorkHours,
+            days: allTaskWorkDays
+          }
+        ];
+
+        var chart = AmCharts.makeChart("chartdiv3", {
+          "type": "serial",
+             "theme": "light",
+          "categoryField": "hours",
+          "rotate": true,
+          "startDuration": 1,
+          "categoryAxis": {
+            "gridPosition": "start",
+            "position": "left"
+          },
+          "colors": ['#FF6600'],
+          "trendLines": [],
+          "graphs": [
+            {
+              "balloonText": "Project is [[value]] WORK hours which is [[description]] days",
+              "fillAlphas": 0.8,
+              "id": "AmGraph-1",
+              "lineAlpha": 0.2,
+              "title": "Tasks",
+              "type": "column",
+              "valueField": "hours",
+              "descriptionField": "days"
+            }
+          ],
+          "guides": [],
+          "valueAxes": [
+            {
+              "id": "ValueAxis-1",
+              "position": "top",
+              "axisAlpha": 0
+            }
+          ],
+          "allLabels": [],
+          "balloon": {},
+          "titles": [],
+          "dataProvider": allTaskWorkHoursObject,
+          "export": {
+            "enabled": true
+          }
+        });
 
         };
 
